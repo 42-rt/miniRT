5cfe06e00adf4773b53886bd79e1819da7583fd0
jkong <jkong@student.42seoul.kr>
2022-08-22 12:25:25 +0900

    조금 클린

--- rt_object_hit_plane.c
+++ rt_object_hit_plane.c
@@ -6,7 +6,7 @@
 /*   By: jkong <jkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 21:08:50 by jkong             #+#    #+#             */
-/*   Updated: 2022/08/22 11:05:22 by jkong            ###   ########.fr       */
+/*   Updated: 2022/08/22 11:31:26 by jkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,44 +31,50 @@ static int	_find_solution(t_list_object *self, t_ray *ray, double *out)
 	return (1);
 }
 
-static void	_calc_uv(t_list_object *self, t_hit *hit, t_vec3 *out)
+static int	_retrieve_size(t_list_object *self, t_vec3 *uv, double size[2])
 {
-	t_vec3	vec_u;
-	t_vec3	vec_v;
-	t_vec3	vec;
-
-	vec3_uv(self->direction, &vec_u, &vec_v);
-	vec = vec3_sub(hit->collision, self->origin);
-	out->x = vec3_dot(vec_u, vec);
-	out->y = vec3_dot(vec_v, vec);
-	out->z = 0.;
-}
-
-static int	_limit(t_list_object *self, t_vec3 *uv)
-{
-	double	size[2];
-
 	size[0] = self->width;
 	size[1] = self->height;
-	if (size[0] == 0. && size[1] == 0.)
+	if (size[0] != 0. && size[1] != 0.)
 	{
-		size[0] = DEFAULT_SIZE;
-		size[1] = DEFAULT_SIZE;
+		if (uv->x < 0 || uv->x >= self->width
+			|| uv->y < 0 || uv->y >= self->height)
+			return (0);
 	}
-	else if (size[0] == 0.)
-		size[0] = self->height;
-	else if (size[1] == 0.)
+	else if (size[0] != 0.)
 	{
 		if (uv->x * uv->x + uv->y * uv->y > self->width)
 			return (0);
 		size[1] = self->width;
 	}
-	else if (uv->x < 0 || uv->x >= self->width
-		|| uv->y < 0 || uv->y >= self->height)
+	else if (size[1] != 0.)
+		size[0] = self->height;
+	else
+	{
+		size[0] = DEFAULT_SIZE;
+		size[1] = DEFAULT_SIZE;
+	}
+	return (1);
+}
+
+static int	_calc_uv_limit(t_list_object *self, t_hit *hit, t_vec3 *out)
+{
+	t_vec3	vec_u;
+	t_vec3	vec_v;
+	t_vec3	vec;
+	double	size[2];
+
+	vec3_uv(self->direction, &vec_u, &vec_v);
+	vec = vec3_sub(hit->collision, self->origin);
+	out->x = vec3_dot(vec_u, vec);
+	out->y = vec3_dot(vec_v, vec);
+	if (!_retrieve_size(self, out, size))
 		return (0);
-	uv->x = uv->x / size[0] - floor(uv->x / size[0]);
-	uv->y = uv->y / size[1] - floor(uv->y / size[1]);
-	uv->z = 0.;
+	out->x /= size[0];
+	out->y /= size[1];
+	out->x -= floor(out->x);
+	out->y -= floor(out->y);
+	out->z = 0.;
 	return (1);
 }
 
@@ -85,8 +91,7 @@ int	plane_hit(void *this_ptr, t_ray *ray, t_hit *out)
 	out->f = vec3_dot(ray->direction, out->normal) < 0;
 	if (!out->f)
 		out->normal = vec3_neg(out->normal);
-	_calc_uv(self, out, &out->uv);
-	if (!_limit(self, &out->uv))
+	if (!_calc_uv_limit(self, out, &out->uv))
 		return (0);
 	out->obj = self;
 	return (1);
